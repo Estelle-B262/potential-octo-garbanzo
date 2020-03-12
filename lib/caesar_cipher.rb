7708e4d481d767de4e9bb527df8f73aebe71fb7c
Estelle-B262 <estelle.ballif@yahoo.com>
2020-03-12 13:02:00 +0100

    Crypto program finished - TDD

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -1,28 +1,32 @@
-def caesar_cipher(string, shift_number)
+def caesar_cipher(string, string_number)
+	if string.is_a?(String) == false
+   		return "Vous devez entrer un texte."
+	elsif
+	     array = string.split(//)
+	     shifted_array = array.map! {|letter| shift(letter, string_number)}
+	     result = shifted_array.join("")
+	     return result
+	end
+end
 
-  if string.is_a?(String) == false
-  return "Vous devez entrer un texte."
-
-  elsif string.is_a?(String) == true
-  letters = string.split(//)
-  letters.each do |letter|
+def shift(letter, string_number)
 
     if letter.ord >= 97 && letter.ord <= 122
-      if letter.ord + shift_number > 122
-        k = (shift_number + letter.ord) - 122
+      if letter.ord + string_number > 122
+        k = (string_number + letter.ord) - 122
         new_letter_ord = 96 + k
         return new_letter_ord.chr
       else
-        return (letter.ord+shift_number).chr
+        return (letter.ord+string_number).chr
       end
 
     elsif letter.ord >= 65 && letter.ord <= 90
-      if letter.ord + shift_number > 90
-        k = (shift_number + letter.ord) - 90
+      if letter.ord + string_number > 90
+        k = (string_number + letter.ord) - 90
         new_letter_ord = 64 + k
         return new_letter_ord.chr
       else
-        return (letter.ord+shift_number).chr
+        return (letter.ord+string_number).chr
       end
 
     elsif letter.ord >= 32 && letter.ord <= 47
@@ -30,9 +34,6 @@ def caesar_cipher(string, shift_number)
 
     end
 
-  end
-
-  end
-
 end
+
 
